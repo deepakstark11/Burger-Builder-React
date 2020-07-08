706c9847869fc8759a131981e72ebf7958aad9dd
Deepak Kumar <deepakramesh2000@gmail.com>
2020-07-08 15:32:09 +0530

    Added Simple Testing Jest/Enzyme

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,7 +26,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 
 import WithErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false,
   };
